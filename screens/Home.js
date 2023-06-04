a122f8172b83a60bd19b20608fcc21887d092d4e
RUZ4KI <rushikate23@gmail.com>
2023-06-04 16:23:20 +0530

    add header in home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,7 @@
 import {Text, SafeAreaView, Image, View} from 'react-native';
 import React, {useLayoutEffect} from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {ChevronDownIcon} from 'react-native-heroicons/outline';
+import {ChevronDownIcon, UserIcon} from 'react-native-heroicons/outline';
 // import deliveryLogo from '../../food_delivery_app/assets/delivery_icon.avif';
 
 const Home = () => {
@@ -14,7 +14,7 @@ const Home = () => {
   }, [navigation]);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white">
       <View className="flex-row pb-4 items-center mx-4 space-x-2">
         <Image
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
@@ -22,13 +22,14 @@ const Home = () => {
             uri: 'https://links.papareact.com/wru',
           }}
         />
-        <View>
+        <View className="flex-1">
           <Text className="font-bold text-xs text-gray-400">Delivery Now!</Text>
           <Text className="font-bold text-xl">
             Current Location
-            <ChevronDownIcon size={20} />
+            <ChevronDownIcon size={20} color="#00CCBB" />
           </Text>
         </View>
+        <UserIcon size={35} color="#00CCBB" />
       </View>
     </SafeAreaView>
   );
